3b202cf4dd3a7bf3af61718b8bff401cefee074e
fxmy <fxmywc@gmail.com>
2016-01-19 23:42:51 +0800

    feizhai factory

--- apps/sample/src/feizhai.erl
+++ apps/sample/src/feizhai.erl
@@ -1,5 +1,6 @@
 -module(feizhai).
 -compile([export_all]).
+%-export([metainfo/0]).
 
 -include_lib("sample/include/feizhai.hrl").
 -include_lib("kvs/include/metainfo.hrl").
@@ -10,3 +11,16 @@ metainfo() ->
 				#table{name=achieves, container=feed, fields=record_info(fields, achieves)},
 				#table{name=achieve_progress, container=feed, fields=record_info(fields, achieve_progress)}
 				 ]}.
+
+-spec new_feizhai() -> {binary(),binary(),calendar:datetime()}.
+new_feizhai() ->
+	PublicToken = new_token(10),
+	PrivateToken = new_token(10),
+	LifeSpan = calendar:gregorian_seconds_to_datetime( calendar:datetime_to_gregorian_seconds( calendar:universal_time()) + wf:config(sample,feizhai_life, 864000)),
+	{PublicToken,PrivateToken,LifeSpan}.
+
+-spec new_token( pos_integer() ) -> binary().
+new_token(Bytes) ->
+	B64 = base64:encode(crypto:strong_rand_bytes(Bytes)),
+	binary:replace(B64, [<<"=">>,<<"/">>,<<"+">>], <<>>, [global]).
+
