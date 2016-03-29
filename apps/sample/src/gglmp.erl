fb08e5bff708ec993dc1304997c065362dd0995c
fxmy <fxmywc@gmail.com>
2016-03-29 17:46:07 +0800

    map UI

--- apps/sample/src/gglmp.erl
+++ apps/sample/src/gglmp.erl
@@ -6,7 +6,7 @@
 main()    -> #dtl{file="gglmp",app=sample, bindings=[{authkey,authkey()}, {presentmap,presentmap()}, {initmapfunc,initmapfunc()}]}.
 authkey() -> "AIzaSyAAbNcNrZoGgi8YMdZ98Z3UGPXxM8PsbBU".
 presentmap() -> "XiaoSuiGu".
-initmapfunc() -> "var map;function "++presentmap()++"() {map = new google.maps.Map(document.getElementById('map'),{center: {lat: 60.192059, lng: 24.945831},zoom: 12});}".
+initmapfunc() -> "var map;function "++presentmap()++"() {map = new google.maps.Map(document.getElementById('map'),{center: {lat: 60.192059, lng: 24.945831},zoom: 12,mapTypeControl:false,fullscreenControl:true});}".
 
 event(init) ->
 	wf:wire("console.log('!!!event init!!!')"),
