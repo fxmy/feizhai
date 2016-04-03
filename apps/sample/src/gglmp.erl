091cd10d14831b01880f2e12d3884876997fffde
fxmy <fxmywc@gmail.com>
2016-04-03 16:15:56 +0800

    button ontop of googlemap

--- apps/sample/src/gglmp.erl
+++ apps/sample/src/gglmp.erl
@@ -3,11 +3,17 @@
 -include_lib("n2o/include/wf.hrl").
 -include_lib("nitro/include/nitro.hrl").
 
-main()    -> #dtl{file="gglmp",app=sample, bindings=[{authkey,authkey()}, {presentmap,presentmap()}, {initmapfunc,initmapfunc()}]}.
+main()    -> #dtl{file="gglmp",app=sample, bindings=[{authkey,authkey()}, {presentmap,presentmap()}, {initmapfunc,initmapfunc()},{body,body()}]}.
+body() -> [#panel{id=map},
+	   #panel{class=["fixed-action-btn"],style=["bottom: 25px; right: 48px;"],body=[
+		#link{id=aaa,class=["btn-floating btn-large waves-effect waves-light red"],postback=btn,body=[
+			#i{class=["material-icons"],body=["add"]}]}
+              ]}].
 authkey() -> "AIzaSyAAbNcNrZoGgi8YMdZ98Z3UGPXxM8PsbBU".
 presentmap() -> "XiaoSuiGu".
 initmapfunc() -> "var map;function "++presentmap()++"() {map = new google.maps.Map(document.getElementById('map'),{center: {lat: 60.192059, lng: 24.945831},zoom: 12,mapTypeControl:false,fullscreenControl:true});}".
 
+event(btn) -> wf:wire("console.log('btn!')");
 event(init) ->
 	wf:wire("console.log('!!!event init!!!')"),
 	wf:info(?MODULE,"~p-> init!~n",[self()]);
