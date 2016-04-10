da1b5ceabd314dfd5d912dd8367aed3026e8dc48
fxmy <fxmywc@gmail.com>
2016-04-10 16:09:52 +0800

    clean up & minor tweak

--- apps/sample/src/gglmp.erl
+++ apps/sample/src/gglmp.erl
@@ -12,41 +12,37 @@ body() -> [#panel{id=map},
 authkey() -> "AIzaSyAAbNcNrZoGgi8YMdZ98Z3UGPXxM8PsbBU".
 presentmap() -> "XiaoSuiGu".
 initmapfunc() -> "var map;function "++presentmap()++"() {map = new google.maps.Map(document.getElementById('map'),{center: {lat: 60.192059, lng: 24.945831},zoom: 12,mapTypeControl:false,fullscreenControl:true});}".
-infoWindowContent() -> wf:to_list(wf:render(#panel{id=wf:state(infowindow),body=[
-%					      #textbox{id=wf:state(achieve)},
-%					      #hidden{id=wf:state(lan),disabled=true},
-%					      #hidden{id=wf:state(lat),disabled=true},
-%					      #hidden{id=wf:state(validt),disabled=true},
-%					      #button{body= <<"成就get"/utf8>>,postback=newachieve}
-					     ]})).
+infoWindowContent() ->
+	wf:to_list(wf:render(#blockquote{body= <<"(*ﾟ∇ﾟ)今日之我已不是昨日之我！(*ﾟ∇ﾟ)"/utf8>>,style=["font-weight: bold; margin-bottom: 0px;"]})) ++ wf:to_list(wf:render(#panel{id=wf:state(infowindow)})).
 tmpidcmpac() -> lists:delete($-, wf:temp_id()).
 
-api_event(Func,Args,_Cx) -> wf:info(?MODULE, "api_event: ~p,~p~n", [Func,Args]),
-			    wf:insert_bottom(wf:state(infowindow),#textbox{id=wf:state(achieve)}),
-			    wf:insert_bottom(wf:state(infowindow),#button{body= <<"成就get"/utf8>>}).
+api_event(Func,Args,_Cx) ->
+	wf:info(?MODULE, "api_event: ~p,~p~n", [Func,Args]),
+	wf:insert_bottom(wf:state(infowindow),#hidden{id=wf:state(validt),disabled=true,value=wf:pickle(wf:state(validt_content))}),
+	wf:insert_bottom(wf:state(infowindow),#textbox{id=wf:state(achieve)}),
+	wf:insert_bottom(wf:state(infowindow),#button{body= <<"成就get"/utf8>>,postback=newachieve,source=[wf:state(achieve),wf:state(validt)],class=["btn waves-effect waves-light"]}).
 
-event(newachieve) -> wf:info(?MODULE,"New Achieve~n",[]);
-event(btn) -> wf:wire("console.log('btn!');"),
-	      wf:state(infowindow,tmpidcmpac()),
-	      wf:state(apiName,tmpidcmpac()),
-	      wf:state(achieve,tmpidcmpac()),
-	      wf:state(validt,tmpidcmpac()),
-	      wf:wire(#api{name=wf:state(apiName)}),
-	      wf:wire("var hndlLctnErr = new Function('a','b','c','b.setPosition(c);b.setContent(a ? \"Error: The Geolocation service failed.\" : \"Error: Your browser doesn`t support geolocation.\");');
-		      console.log('0');
+event(newachieve) ->
+	wf:info(?MODULE,"New Achieve:~p,~p,~p~n",[wf:q(wf:state(achieve)),wf:depickle(wf:q(wf:state(validt))),wf:state(validt_content)]);
+event(btn) ->
+	wf:wire("console.log('btn!');"),
+	wf:state(infowindow,tmpidcmpac()),
+	wf:state(apiName,tmpidcmpac()),
+	wf:state(achieve,tmpidcmpac()),
+	wf:state(validt,tmpidcmpac()),
+	wf:state(validt_content,crypto:rand_bytes(4)),
+	wf:wire(#api{name=wf:state(apiName)}),
+	wf:wire("var hndlLctnErr = new Function('a','b','c','b.setPosition(c);b.setContent(a ? \"Error: The Geolocation service failed.\" : \"Error: Your browser doesn`t support geolocation.\");');
 var infoWindow = new google.maps.InfoWindow({map: map});
-		      console.log('1');
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-		      console.log('2');
       var pos = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
-		      console.log('3');
 
       infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found."++infoWindowContent()++"');
+      infoWindow.setContent('"++infoWindowContent()++"');
       map.setCenter(pos);
       "++wf:state(apiName)++"(pos);
     }, function() {
