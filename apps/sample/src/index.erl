1eb1126b89fab43d211d5e47003e827e893d0363
fxmy <fxmywc@gmail.com>
2016-05-01 16:58:12 +0800

    {client, DATA} test

--- apps/sample/src/index.erl
+++ apps/sample/src/index.erl
@@ -13,6 +13,7 @@ body()    -> [#panel{class=["fixed-action-btn"],style=["bottom: 25px; right: 25p
 	      #panel{id=history},
 	      #textbox{id=message},
 	      #button{id=send,body="Chat",postback=chat,source=[message]},
+	      #button{id=tz, body="get timezone", postback=tz},
 		%#textbox{id=nitro:temp_id(),disabled=true,style=["visibility: hidden;"]},
 		%#button{body="crdrcd"},
 		#hidden{id=kkk,disabled=true},
@@ -23,10 +24,12 @@ api_event(Func, Args, _Cx) ->
 	wf:info(?MODULE,"API EVENT: ~p,~p~n",[Func,Args]).
 
 event(init) -> wf:reg(room);
+event(tz) -> wf:wire("ws.send(enc(tuple(atom('client'), tuple(bin('timezone'), number(new Date().getTimezoneOffset())))));");
 event(chat) -> wf:send(room,{client,{peer(),message()}});
 event(btn) ->  wf:wire(#api{name=apiName,tag=apiTag});%,delegate=apiDelegate});
 event(kkk) -> wf:wire(#jq{target=kkk,property=value,right=message()}),
 	wf:wire(#jq{target=qqq,property=value,right=#jq{target=kkk,property=value}});
 	%wf:wire(#jq{target=qqq,property=value,right=#jq{target=kkk,property=value}});
+event({client, {<<"timezone">>, T}}) -> wf:info(?MODULE, "tz-> ~p", [T]);
 event({client,{P,M}}) -> wf:insert_bottom(history,#panel{id=history,body=[P,": ",M,#br{}]});
 event(Event) -> wf:info(?MODULE,"Unknown Event: ~p~n",[Event]).
